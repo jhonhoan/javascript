1ce2fe5480d9bcb83cb172a77fee5314d8caabe8
jhonatan <132399356+jhonhoan@users.noreply.github.com>
2023-06-15 16:52:07 -0300

    Condições JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60.5
+console.log(`A velocidade do ceu carro é ${vel}Km/h`)
+
+if (vel > 60){
+console.log('Você ultrapssou a velocidade permitida (50Km/h). MULTADO!!')
+
+}
+
+
+console.log('Dirija sempre usando o cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'Brasil'
+var nome = 'Brasileiro'
+var nome2 = 'Americano'
+console.log(`Vivendo no país ${pais}`)
+if (pais == 'Brasil'){
+
+    console.log(`Você é ${nome} `)
+} else{
+    console.log(`Você é ${nome2} `)
+}
