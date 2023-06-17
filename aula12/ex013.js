410f23627900431489bf7b39321e378aee4a5017
jhonatan <132399356+jhonhoan@users.noreply.github.com>
2023-06-16 21:18:48 -0300

    Condições parte 2

---
+++ aula12/ex013.js
@@ -0,0 +1,30 @@
+var agora = new Date()
+var DiaSem = agora.getDay()
+
+
+switch( DiaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça-Feira')
+        break
+    case 3:
+        console.log('Quarta-Feira')
+        break
+    case 4:
+        console.log('Quinta-Feira')
+        break
+    case 5:
+        console.log('Sexta-Feira')
+        break
+    case 6:
+        console.log('Sabado')
+    
+    default:
+        console.log('Dia invalido')
+        break
+}
